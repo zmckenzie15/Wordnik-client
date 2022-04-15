964b46f244b2543f59b7c61600695323a75fce4f
zoemckenzie <zmckenzie82@gmail.com>
2022-04-15 10:22:43 -0700

    Add new query

--- src/edu/mills/cs180a/Main.java
+++ src/edu/mills/cs180a/Main.java
@@ -3,8 +3,14 @@ package edu.mills.cs180a;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.mills.cs180a.wordnik.client.api.WordApi;
 import edu.mills.cs180a.wordnik.client.api.WordsApi;
 import edu.mills.cs180a.wordnik.client.invoker.ApiClient;
+import edu.mills.cs180a.wordnik.client.model.Definition;
+import edu.mills.cs180a.wordnik.client.model.WordObject;
 import edu.mills.cs180a.wordnik.client.model.WordOfTheDay;
 
 public class Main {
@@ -23,9 +29,22 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        ApiClient client = new ApiClient("api_key", getApiKey());
-        WordsApi wordsApi = client.buildClient(WordsApi.class);
-        WordOfTheDay word = wordsApi.getWordOfTheDay("2022-03-15");
-        System.out.println(word);
+//        ApiClient client = new ApiClient("api_key", getApiKey());
+//        WordsApi wordsApi = client.buildClient(WordsApi.class);
+//        WordOfTheDay word = wordsApi.getWordOfTheDay("2022-03-15");
+//        System.out.println(word);
+    	
+    	ApiClient client = new ApiClient("api_key", getApiKey());
+    	WordsApi wordsApi = client.buildClient(WordsApi.class);
+        WordApi wordApi = client.buildClient(WordApi.class);
+        
+        WordObject randomWord = wordsApi.getRandomWord("true", "noun", null, null, -1, 1, -1, 3, -1);
+        System.out.println(randomWord);
+        
+        List<String> sourceDictionaries = new ArrayList<String>();
+        sourceDictionaries.add("all");
+       
+        List<Definition> definition = wordApi.getDefinitions(randomWord.getWord(), 20, "noun", "false", sourceDictionaries, "false", "false");
+        System.out.println(definition);
     }
 }
